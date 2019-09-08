aea89ce959d3667929418df49d3da4c97e24a560
filippo savi <filssavi@gmail.com>
2019-09-08 16:16:26 +0200

    Removed MainAppBody as it was useless

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React, {Component} from 'react';
-import MainAppBody from "./components/MainAppBody";
+import React from 'react';
 
-import { Provider } from "react-redux";
+import {Tab, Tabs} from "react-bootstrap";
+import TabContent from "./components/TabContent";
+import {useSelector} from "react-redux";
+
 
 import './App.css';
 
-import store from "./store";
-
-class App extends Component {
-
-  render() {
+let App =  props => {
+  const tabs = useSelector(
+      state => state.tabs
+  );
+  const settings = useSelector(
+      state => state.settings
+  );
     return (
-        <Provider store={store}>
-          <div className="App">
-            <MainAppBody />
-          </div>
-        </Provider>
+      <div className="App">
+        <Tabs defaultActiveKey={settings.default_tab} id="uncontrolled-tab-example">
+          {tabs.map((tab) => {
+            return(
+                <Tab eventKey={tab.name} title={tab.name}> <TabContent tab={tab}/></Tab>
+            )
+          })}
+        </Tabs>
+      </div>
     );
-  }
-}
+};
 
 export default App;
+
+
