09222a2ecf712a7c8313279f4c12bc9a5a681973
filippo savi <filssavi@gmail.com>
2019-09-21 11:03:59 +0200

    Added register form preview to tab creator

--- src/components/Modal_Components/tabCreatorRegisterModal.js
+++ src/components/Modal_Components/tabCreatorRegisterModal.js
@@ -21,20 +21,44 @@ const mapDispatchToProps = dispatch => {
 class TabCreatorRegisterModal extends Component {
     constructor(props){
         super(props);
-        this.state = {enable_word_fields:false};
+        this.state = {reg_direction:'', enable_word_fields:false,single_type_checked:true,words_type_checked:false, reg_type:'single'};
     }
 
 
     handleChange = (event) => {
-        debugger;
         if(event.target.name==='reg_direction') {
-            this.setState({[event.target.name]: event.target.id});
+            if(event.target.id==='read'){
+
+                if(this.state.reg_direction.includes('W')){
+                    if(event.target.checked){
+                        this.setState({reg_direction: 'R/W'});
+                    } else {
+                        this.setState({reg_direction: 'W'});
+                    }
+
+                } else  {
+                    this.setState({reg_direction: 'R'});
+                }
+            } else{
+                if(this.state.reg_direction.includes('R')){
+                    if(event.target.checked){
+                        this.setState({reg_direction: 'R/W'});
+                    } else {
+                        this.setState({reg_direction: 'R'});
+                    }
+                } else  {
+                    this.setState({reg_direction: 'W'});
+                }
+            }
+
         }else if(event.target.name==='reg_type'){
             this.setState({[event.target.name]: event.target.id});
             if(event.target.id==='words'){
-                this.setState({enable_word_fields:true})
+                this.setState({enable_word_fields:true});
+                this.setState({single_type_checked:false,words_type_checked:true});
             } else{
-                this.setState({enable_word_fields:false})
+                this.setState({enable_word_fields:false});
+                this.setState({single_type_checked:true,words_type_checked:false});
             }
 
         }else {
@@ -43,8 +67,22 @@ class TabCreatorRegisterModal extends Component {
 
     };
 
-    handleClose = (event) =>{
+    handleClose = () =>{
 
+        let register = {};
+        register['register_name'] = this.state.reg_name;
+        register['offset'] = this.state.reg_offset;
+        register['description'] = this.state.reg_description;
+        register['direction'] = this.state.reg_direction;
+        register['register_format'] = this.state.reg_type;
+        if(this.state.enable_word_fields){
+            let field_names = this.state.field_names.split('\n');
+            let field_desc = this.state.field_desc.split('\n');
+            register['field_names'] = field_names;
+            register['field_descriptions'] = field_desc;
+        }
+        this.props.done(register);
+        this.props.hideModal();
     };
 
     handleHide = () => {
@@ -62,16 +100,17 @@ class TabCreatorRegisterModal extends Component {
                     <Form>
                         <Form.Group>
                             <Form.Control inline name='reg_name' placeholder="Register Name" type="text" onChange={this.handleChange} />
+                            <Form.Control name='reg_offset' placeholder="Address offset" type="text" onChange={this.handleChange} />
+                            <Form.Control name='reg_description' placeholder="Description" type="text" onChange={this.handleChange} />
                             <Form.Group>
                                 <Form.Label>Register access capabilities</Form.Label>
                                 <Form.Check label="Read" name="reg_direction" type="checkbox" id={'read'} onChange={this.handleChange} />
                                 <Form.Check label="Write" name="reg_direction" type="checkbox" id={'write'} onChange={this.handleChange} />
                             </Form.Group>
-                            <Form.Control name='reg_offset' placeholder="Address offset" type="text" onChange={this.handleChange} />
                             <div>
                                 <Form.Label>Register type</Form.Label>
-                                <Form.Check name="reg_type" label="single" type="radio" id='single' onChange={this.handleChange} />
-                                <Form.Check name="reg_type" label="words" type="radio" id='words' onChange={this.handleChange} />
+                                <Form.Check name="reg_type" label="single" type="radio" id='single' checked={this.state.single_type_checked} onChange={this.handleChange} />
+                                <Form.Check name="reg_type" label="words" type="radio" id='words' checked={this.state.words_type_checked} onChange={this.handleChange} />
                             </div>
                             <Form.Group>
                                 <Form.Label>Field Names</Form.Label>
--- src/components/Tab_creator/TabCreator.js
+++ src/components/Tab_creator/TabCreator.js
@@ -1,10 +1,12 @@
 import React, {Component}  from 'react';
-import {Col, Image, Row} from "react-bootstrap";
+import {Col, Form, Image, Row} from "react-bootstrap";
 
 import TabCreatorRegisterModal from "../Modal_Components/tabCreatorRegisterModal"
 import TabCreatorImageChooser from "../Modal_Components/tabCreatorImageChooser";
 import {showModal} from "../../redux/Actions/modalsActions";
 import {connect} from "react-redux"
+import SingleValueField from "../Common_Components/SingleValueField";
+import TwoValuesField from "../Common_Components/TwoValuesField";
 
 
 function mapStateToProps(state) {
@@ -22,7 +24,7 @@ const mapDispatchToProps = dispatch => {
 class TabCreator extends Component {
     constructor(props) {
         super(props);
-        this.state= {tab_image:"assets/Icons/add_peripheral_img.svg"};
+        this.state= {tab_image:"assets/Icons/add_peripheral_img.svg", tab_registers:[]};
     }
 
 
@@ -48,8 +50,8 @@ class TabCreator extends Component {
         reader.readAsDataURL(file);
     };
 
-    handleRegisterCreationDone = () => {
-
+    handleRegisterCreationDone = (register) => {
+        this.setState({tab_registers:[...this.state.tab_registers, register]});
     };
 
     render(){
@@ -66,7 +68,30 @@ class TabCreator extends Component {
                     </Col>
                     <Col>
                         <div id={"tab_creator_add_register_col"}>
-                            <Image src="assets/Icons/add_register.svg" id="addRegister" alt='add register' onClick={this.handleClick}fluid/>
+                            <Row>
+                                <Form>
+                                    {this.state.tab_registers.map((reg, i) => {
+                                        debugger;
+                                        if(reg.register_format === "single"){
+                                            return(
+                                                <SingleValueField key={i} name={reg.register_name} value={reg.value} description={reg.description}/>
+                                            );
+                                        } else if(reg.register_format === "complex"){
+                                            return(
+                                                <SingleValueField key={i} name={reg.register_name} value={reg.value} description={reg.description}/>
+                                            );
+                                        } else if(reg.register_format==='words'){
+                                            let split_values = [(reg.value & 0x0000ffff), (reg.value & 0xffff0000) >> 16];
+                                            return(
+                                                <TwoValuesField key={i} field_names={reg.field_names} register_name={reg.register_name} value={split_values} field_descriptions={reg.field_descriptions}/>
+                                            );
+                                        } else return(<p>invalid form field</p>);
+                                    })}
+                                </Form>
+                            </Row>
+                            <Row>
+                                <Image src="assets/Icons/add_register.svg" id="addRegister" alt='add register' onClick={this.handleClick}fluid/>
+                            </Row>
                         </div>
                     </Col>
                 </Row>
