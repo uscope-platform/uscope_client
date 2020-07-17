af1c4a96da4aa822e42e7b8a2a0fe9dfcce06f7a
filssavi <filippo.savi@nottingham.edu.cn>
2020-07-17 11:57:45 +0200

    Changed font to monserrat

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import store, {persistor} from "./store";
 import {PersistGate} from "redux-persist/lib/integration/react";
 import {BrowserRouter} from "react-router-dom";
 
+import './fonts/montserrat-v14-latin-ext_latin-100.woff2'
+import './fonts/montserrat-v14-latin-ext_latin-200.woff2'
+import './fonts/montserrat-v14-latin-ext_latin-300.woff2'
+import './fonts/montserrat-v14-latin-ext_latin-regular.woff2'
+import './fonts/montserrat-v14-latin-ext_latin-500.woff2'
 
 ReactDOM.render(
     <Provider store={store}>
