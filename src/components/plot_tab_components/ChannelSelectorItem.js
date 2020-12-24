38e162d64019e4529cb74469f56b8c7e101b88c9
filippo savi <filippo.savi@nottingham.edu.cn>
2020-12-24 17:03:26 +0000

    fixed initial channel selector item status

--- src/components/plot_tab_components/ChannelSelectorItem.js
+++ src/components/plot_tab_components/ChannelSelectorItem.js
@@ -8,17 +8,15 @@ const Centering = styled.div`
 `
 
 let  ChannelSelectorItem = props => {
-    const [enabled, set_enabled] = useState(false);
 
     function handleChannelStateChange(event){
-        set_enabled(event.target.checked)
         let channel_status = {id:event.target.name, status:event.target.checked}
         props.onStatusChange(channel_status);
     }
 
     return(
         <Centering>
-            <Checkbox style={{marginRight:'0.5rem'}} name={props.id} onChange={handleChannelStateChange} value={enabled} label={props.name}/>
+            <Checkbox style={{marginRight:'0.5rem'}} name={props.id} onChange={handleChannelStateChange} value={props.value} label={props.name}/>
         </Centering>
     );
 }
