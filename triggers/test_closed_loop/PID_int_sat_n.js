9cc1e9b79099606c7b9a76cde4e3719ed0312f80
Filippo Savi <filisavi@unimore.it>
2021-09-17 13:00:58 +0200

    Added license and credits

--- triggers/test_closed_loop/PID_int_sat_n.js
+++
@@ -1,9 +0,0 @@
-function pid_int_sat_min_cl(parameters, context) {
-
-
-    let registers = {};
-
-    registers['PID.int_sat_n'] = parameters;
-
-    return {workspace:null, registers:registers};
-}
--- triggers/test_closed_loop/fsw.js
+++
@@ -1,9 +0,0 @@
-function fsw_cl(parameters, context) {
-
-
-    let workspace = {};
-
-    workspace['fsw'] = parameters;
-
-    return {workspace:workspace, registers:null};
-}
