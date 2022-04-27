ff9d0553e3f0621786f27ba2182365317823c0a6
Filippo Savi <filisavi@unimore.it>
2022-04-27 21:17:25 +0200

    added tests for platform proxy

--- test/client_core/mock/server.js
+++ test/client_core/mock/server.js
@@ -21,8 +21,9 @@ import {scripts_api} from "./scripts_api";
 import {programs_api} from "./programs_api";
 import {peripherals_api} from "./peripherals_api";
 import {auth_api} from "./auth_api";
+import {platform_api} from "./platform_api";
 
 
 export const server = setupServer(...cache_handlers, ...applications_api,
     ...bitstreams_api, ...scripts_api, ...programs_api, ...peripherals_api,
-    ...auth_api)
+    ...auth_api, ...platform_api)
