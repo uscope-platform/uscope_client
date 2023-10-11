ff146b3270b97ed1583d3eb5e2bff11b25c21626
filippo savi <filssavi@gmail.com>
2023-10-11 23:35:06 +0200

    Implemented automated versioning of uscope client

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
+import * as child from "child_process";
+
+const commitHash = child.execSync('git rev-parse --short HEAD').toString().trimEnd();
+
 export default defineConfig({
     server:{
         port:3004
@@ -16,6 +20,9 @@ export default defineConfig({
     build:{
         outDir:"build",
         sourcemap:true
+    },
+    define: {
+        __USCOPE_CLIENT_VERSION__: JSON.stringify(commitHash),
     },
     test: {
         globals: true,
