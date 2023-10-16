0bb5dd81f360b7378477469904629f728ee52cc7
filippo savi <filssavi@gmail.com>
2023-10-16 22:48:56 +0200

    added reactflow for emulator UI

--- src/components/Emulator/FcoreEmulationEditor.jsx
+++ src/components/Emulator/FcoreEmulationEditor.jsx
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React from 'react';
+import React, {useCallback} from 'react';
 
 import {UIPanel, SimpleContent} from "../UI_elements";
 
@@ -21,7 +21,28 @@ import {Responsive, WidthProvider} from "react-grid-layout";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
+import ReactFlow, { useNodesState, useEdgesState, addEdge }  from 'reactflow';
+import 'reactflow/dist/style.css';
+
+
+
+const initialNodes = [
+    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+
+
+
 let FcoreEmulationEditor = function (props) {
+
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+    const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+
+
     return(
         <ResponsiveGridLayout
             className="layout"
@@ -32,7 +53,15 @@ let FcoreEmulationEditor = function (props) {
         >
             <UIPanel key="ch_selector" data-grid={{x: 0, y: 0, w: 20, h: 20, static: true}} level="level_2">
                 <SimpleContent name="Channel Selector" content={
-                    <p>Test Emulator</p>
+                    <div style={{ width: '100vw', height: '100vh' }}>
+                        <ReactFlow
+                            nodes={nodes}
+                            edges={edges}
+                            onNodesChange={onNodesChange}
+                            onEdgesChange={onEdgesChange}
+                            onConnect={onConnect}
+                        />
+                    </div>
                 }/>
             </UIPanel>
         </ResponsiveGridLayout>
