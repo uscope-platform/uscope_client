uscope-platform/uscope_client

commit 3c3c87537077313e5b62b82e722d4672c1965ebe

Do selection key deconfliction upon import

src/components/Sidebar/SidebarBase.jsx
@@ -17,11 +17,7 @@ import React, {useEffect} from 'react';
 
 import {useDispatch, useSelector} from "react-redux";
 
-import {
-    SelectableList,
-    SimpleContent,
-    UIPanel
-} from "../UI_elements";
+import {SelectableList, SimpleContent, UIPanel} from "../UI_elements";
 import {Responsive, WidthProvider} from "react-grid-layout";
 
 import 'react-tooltip/dist/react-tooltip.css'
@@ -83,7 +79,19 @@ let  SidebarBase = props =>{
             if(props.onImport){
                 props.onImport(item.data);
             } else {
+                let ids = Object.values(props.objects).map(a => a[props.selection_key]);
                 let obj = new props.template(JSON.parse(item.data));
+                if(ids.includes(obj[props.selection_key])){
+                    if(JSON.stringify(props.objects[obj[props.selection_key]]) === JSON.stringify(obj._get_program())){
+                        return;
+                    } else {
+                        obj.id = get_next_id(Object.values(props.objects).map(a => a[props.selection_key]).sort());
+                        let labels = Object.values(props.objects).map(a => a[props.display_key]);
+                        if(labels.includes(obj[props.display_key])){
+                            obj[props.display_key] = obj[props.display_key] + "_" + obj.id.toString();
+                        }
+                    }
+                }
                 obj.add_remote().then();
             }
         }).catch((err)=>{

commit 9926a2d0e77a592ffe1aae98bfd6bae4767fdbc0

Fix import bug introduced in 3c3c875

src/client_core/data_models/up_application.js
@@ -421,6 +421,10 @@ export class up_application {
         })
     }
 
+    get_raw_obj = () => {
+        return this._get_app();
+    }
+
     _get_app = () =>{
         let misc_params = up_application._get_nonstandard_fields(this);
         let ret_obj = {

src/client_core/data_models/up_bitstream.js
@@ -66,6 +66,10 @@ export class up_bitstream {
         })
     }
 
+    get_raw_obj = () => {
+        return this._get_bitstream();
+    }
+
     _get_bitstream = () =>{
         return {
             id: this.id,

src/client_core/data_models/up_emulator.js
@@ -431,6 +431,10 @@ export class up_emulator {
         })
     }
 
+    get_raw_obj = () => {
+        return this._get_emulator();
+    }
+
     _get_emulator = () =>{
         return {
             id: this.id,

src/client_core/data_models/up_filter.js
@@ -85,6 +85,10 @@ export class up_filter {
         })
     }
 
+    get_raw_obj = () => {
+        return this._get_filter();
+    }
+
     _get_filter = () =>{
         return {
                 id: this.id,

src/client_core/data_models/up_peripheral.js
@@ -205,6 +205,10 @@ export class up_peripheral {
         return backend_get(api_dictionary.peripherals.direct_read + '/' +address);
     }
 
+    get_raw_obj = () => {
+        return this._get_periph();
+    }
+
     _get_periph = () =>{
         let cleaned_registers = [];
         for(let i of this.registers){

src/client_core/data_models/up_program.js
@@ -74,6 +74,10 @@ export class up_program {
         })
     }
 
+    get_raw_obj = () => {
+        return this._get_program();
+    }
+
     _get_program = () =>{
         return {
             id: this.id,

src/client_core/data_models/up_register.js
@@ -182,6 +182,11 @@ export class up_register {
         return f_names
     }
 
+    get_raw_obj = () => {
+        return this._get_register();
+    }
+
+
     _get_register = () => {
         let converted_fields = [];
         for(let i of this.fields){

src/client_core/data_models/up_script.js
@@ -55,6 +55,10 @@ export class up_script {
         })
     }
 
+    get_raw_obj = () => {
+        return this._get_script();
+    }
+
     _get_script = () =>{
         return {
                 id: this.id,

src/components/Sidebar/SidebarBase.jsx
@@ -82,7 +82,7 @@ let  SidebarBase = props =>{
                 let ids = Object.values(props.objects).map(a => a[props.selection_key]);
                 let obj = new props.template(JSON.parse(item.data));
                 if(ids.includes(obj[props.selection_key])){
-                    if(JSON.stringify(props.objects[obj[props.selection_key]]) === JSON.stringify(obj._get_program())){
+                    if(JSON.stringify(props.objects[obj[props.selection_key]]) === JSON.stringify(obj.get_raw_obj())){
                         return;
                     } else {
                         obj.id = get_next_id(Object.values(props.objects).map(a => a[props.selection_key]).sort());