11e2a593e3935979d7a142566b519125e5423579
filippo savi <filssavi@gmail.com>
2024-06-23 19:42:04 +0200

    Purged react grid layout from plot tab

--- src/components/plot_tab_components/PlotTab.jsx
+++ src/components/plot_tab_components/PlotTab.jsx
@@ -22,51 +22,61 @@ import ParametersArea from "./ParametersArea";
 import MacroActions from "./MacroActions";
 import {ColorTheme, UIPanel, SimpleContent} from "../UI_elements";
 import TerminalComponent from "./Terminal";
-import {Responsive, WidthProvider} from "react-grid-layout";
 
-const ResponsiveGridLayout = WidthProvider(Responsive);
 
 let PlotTab = function (props) {
     const channels = useSelector(state => state.channels);
 
         return(
-            <ResponsiveGridLayout
-                className="layout"
-                breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-                cols={{ lg: 24, md: 20, sm: 12, xs: 8, xxs: 4 }}
-                rowHeight={30}
-                useCSSTransforms={false}
-            >
-                <UIPanel key="ch_selector" data-grid={{x: 0, y: 0, w: 3, h: 10, static: true}} level="level_2">
-                    <SimpleContent name="Channel Selector" content={
-                        <ChannelSelector channels={channels}/>
-                    }/>
-                </UIPanel>
-                <UIPanel key="scope" data-grid={{x: 3, y: 0, w: 21, h: 10, static: true}} level="level_2">
-                    <SimpleContent name="Scope" content={
-                        <PlotComponent palette={{colorway:ColorTheme.plot_palette}} refreshRate={125}/>
-                    }/>
-                </UIPanel>
-                <UIPanel key="parameters" data-grid={{x: 0, y: 10, w: 4, h: 7, static: true}} level="level_2">
-                    <SimpleContent name="Parameters" content={
-                        <ParametersArea />
-                    }/>
-                </UIPanel>
-                <UIPanel key="macro" data-grid={{x: 4, y: 10, w: 20, h: 7, static: true}} level="level_2">
-                    <SimpleContent name="Macro" content={
-                        <MacroActions />
-                    }/>
-                </UIPanel>
-                <UIPanel key="terminal" data-grid={{x:1, y: 17, w: 20, h: 8, static: true}} level="level_2">
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 10,
+                height: "100%"
+            }}>
+                <div style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: 10,
+                    width:"100%"
+                }}>
+                    <UIPanel key="ch_selector"  style={{flexGrow:0.5}} level="level_2">
+                        <SimpleContent name="Channel Selector" content={
+                            <ChannelSelector channels={channels}/>
+                        }/>
+                    </UIPanel>
+                    <UIPanel key="scope" style={{flexGrow:1}} level="level_2">
+                        <SimpleContent name="Scope" content={
+                            <PlotComponent palette={{colorway: ColorTheme.plot_palette}} refreshRate={125}/>
+                        }/>
+                    </UIPanel>
+                </div>
+
+                <div style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: 10,
+                    minHeight:"300px"
+                }}>
+                    <UIPanel style={{flexGrow:0.4}} key="parameters" level="level_2">
+                        <SimpleContent name="Parameters" content={
+                            <ParametersArea/>
+                        }/>
+                    </UIPanel>
+                    <UIPanel style={{flexGrow:1}} key="macro" level="level_2">
+                        <SimpleContent name="Macro" content={
+                            <MacroActions/>
+                        }/>
+                    </UIPanel>
+                </div>
+                <UIPanel key="terminal" level="level_2">
                     <SimpleContent name="Terminal" content={
                         <TerminalComponent/>
                     }/>
                 </UIPanel>
-            </ResponsiveGridLayout>
+            </div>
         );
 };
-
-
 
 
 export default PlotTab;
