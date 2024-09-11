0049c34bf04357cf691e01a92ae700e4b987a813
filippo savi <filssavi@gmail.com>
2024-09-11 21:21:52 +0200

    rename scalar_io to common_io

--- src/components/Views/Emulators/Sidebar/NodesSidebar/CoreInputProperties.jsx
+++ src/components/Views/Emulators/Sidebar/NodesSidebar/CoreInputProperties.jsx
@@ -165,7 +165,7 @@ let  CoreInputProperties = props =>{
             ret.push(<Checkbox name='signed' value={sel_in.signed} onChange={handle_change_signed} label="Signed"/>);
         }
 
-        ret.push(<Checkbox name='scalar_io' value={sel_in.scalar_io} onChange={handle_change_signed} label="Scalar IO"/>);
+        ret.push(<Checkbox name='common_io' value={sel_in.common_io} onChange={handle_change_signed} label="Scalar IO"/>);
         return ret;
     }
 
