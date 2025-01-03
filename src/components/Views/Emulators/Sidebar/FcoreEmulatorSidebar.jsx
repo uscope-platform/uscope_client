a69cdefc17d6e992fe65ca4da89be334d0a25fc7
filippo savi <filssavi@gmail.com>
2025-01-03 11:23:17 +0100

    Added infrastructure for emulator progress panel

--- src/components/Views/Emulators/Sidebar/FcoreEmulatorSidebar.jsx
+++ src/components/Views/Emulators/Sidebar/FcoreEmulatorSidebar.jsx
@@ -26,6 +26,7 @@ import WarningsPanel from "./WarningsPanel";
 import HilControl from "./HilControl";
 import AsmSelector from "./AsmSelector.jsx";
 import BreakpointsPanel from "./BreakpointsPanel.jsx";
+import ProgressPanel from "../FcoreDebugger/ProgressPanel.jsx";
 
 let  FcoreEmulatorSidebar = props =>{
 
@@ -116,6 +117,9 @@ let  FcoreEmulatorSidebar = props =>{
                 on_add={props.on_add_breakpoint}
                 on_remove={props.on_remove_breakpoint}
                 selected_program={props.selections.program}
+                enable={props.selections.tab === 3}
+            />
+            <ProgressPanel
                 enable={props.selections.tab === 3}
             />
         </div>
