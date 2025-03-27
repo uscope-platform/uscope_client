d9a8d51c9dc884f4aaf54153e8fb8ef849a07e3c
filippo savi <filssavi@gmail.com>
2025-03-27 21:30:34 +0100

    get app working with npm and remove unused dependencies

--- src/components/UI_elements/index.jsx
+++ src/components/UI_elements/index.jsx
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export {StyledScrollbar} from "./StyledScrollbar"
 export {SelectField} from "./Select"
 export {Label} from "./Label"
 export {InputField} from "./InputField"
