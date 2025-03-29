8df6fb5ec9c70f67e391f972956e261277ad4fc3
filippo savi <filssavi@gmail.com>
2025-03-29 11:51:00 +0100

    updates to redux

--- src/redux/Actions/types.js
+++ src/redux/Actions/types.js
@@ -13,8 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//                   PARAMETERS ACTIONS
-export const SAVE_PARAMETER =  'SEND_PARAMETER';
 //                   REGISTERS ACTIONS
 export const LOAD_REGISTERS = 'LOAD_REGISTERS';
 export const SEND_REGISTER = 'SEND_REGISTER';
@@ -31,28 +29,7 @@ export const UPSERT_FIELD = 'UPSERT_FIELD';
 export const REMOVE_PERIPHERAL = 'REMOVE_PERIPHERAL';
 export const REMOVE_REGISTER = 'REMOVE_REGISTER';
 export const REMOVE_FIELD = 'REMOVE_FIELD';
-//                  APPLICATIONS ACTIONS
-export const LOAD_APPLICATIONS = 'LOAD_APPLICATIONS';
-export const ADD_APPLICATION = 'ADD_APPLICATION';
-export const REMOVE_APPLICATION = 'REMOVE_APPLICATION';
-export const UPDATE_APPLICATION = 'UPDATE_APPLICATION';
 //                  FCORE PROGRAMS ACTIONS
 export const ADD_PROGRAM = "ADD_PROGRAM";
 export const REMOVE_PROGRAM = "REMOVE_PROGRAM";
 export const LOAD_ALL_PROGRAMS = "LOAD_ALL_PROGRAM";
-//                  FILTER ACTIONS
-export const ADD_FILTER = "ADD_FILTER";
-export const REMOVE_FILTER = "REMOVE_FILTER";
-export const LOAD_ALL_FILTERS = "LOAD_ALL_FILTERS";
-
-//                  EMULATOR ACTIONS
-export const ADD_EMULATOR = "ADD_EMULATOR";
-export const REMOVE_EMULATOR = "REMOVE_EMULATOR";
-export const LOAD_ALL_EMULATOR = "LOAD_ALL_EMULATOR";
-export const UPDATE_EMULATOR = "UPDATE_EMULATOR";
-
-
-//                  BITSTREAMS
-export const ADD_BITSTREAM = "ADD_BITSTREAM";
-export const REMOVE_BITSTREAM = "REMOVE_BITSTREAM";
-export const LOAD_ALL_BITSTREAMS = "LOAD_ALL_BITSTREAMS";
