39dda0135e2e807df2f34ab0aa7407558729ff58
filippo savi <filssavi@gmail.com>
2025-10-21 14:24:11 +0100

    forgot to add unversioned file to previous commit

---
+++ src/interfaces/plot_view.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright(c) 2025. Filippo Savi
+ * Author: Filippo Savi <filssavi@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+import type {channel} from "#interfaces/index.js";
+
+export interface plot_channel{
+    x: number[],
+    y: number[],
+    type: string,
+    mode: string,
+    name: string,
+    visible: boolean,
+    spec: channel
+}
