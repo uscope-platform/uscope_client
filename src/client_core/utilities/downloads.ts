b0af5ca4ab82cf1d69a76be676061c701e12e6c4
Filippo Savi <filisavi@unimore.it>
2025-11-18 11:20:27 +0100

    Add timebase to the HIL downloaded file

--- src/client_core/utilities/downloads.ts
+++ src/client_core/utilities/downloads.ts
@@ -28,20 +28,21 @@ export let download_json = (content: any, filename: string) =>{
 }
 
 export let download_plot = (channels: any, file_name: string) => {
+    let timebase = channels[0].x;
     let data = channels.map((ch : any)=>{
         return ch.y;
     });
     let csv_content;
 
-    csv_content = `${channels[0].name},${channels[1].name},${channels[2].name},${channels[3].name},${channels[4].name},${channels[5].name}\n`
+    csv_content = `timebase,${channels[0].name},${channels[1].name},${channels[2].name},${channels[3].name},${channels[4].name},${channels[5].name}\n`
     for(let i = 0; i<data[0].length; i++){
 
-        csv_content += `${data[0][i]},${data[1][i]},${data[2][i]},${data[3][i]},${data[4][i]},${data[5][i]}\n`
+        csv_content += `${timebase[i]},${data[0][i]},${data[1][i]},${data[2][i]},${data[3][i]},${data[4][i]},${data[5][i]}\n`
     }
 
 
     let [month, day, year]    = new Date().toLocaleDateString("en-US").split("/");
-    let [hour, minute, second] = new Date().toLocaleTimeString("en-US").split(/:| /);
+    let [hour, minute, second] = new Date().toLocaleTimeString("en-US").split(/[: ]/);
     let filename = file_name.replace(' ', '_')+ '_'+ day+month+year+hour+minute+second;
 
     const encodedUri = encodeURI('data:text/csv;charset=utf-8,' + csv_content);
